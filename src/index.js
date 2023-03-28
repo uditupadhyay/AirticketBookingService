87186c2c6aef2c6f5553c5ab50d97ace980acf29
uditmar24 <uditmar24@gmail.com>
2023-03-29 02:27:30 +0530

    added error files

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const setupAndStartServer= ()=>{
         if(process.env.DB_SYNC){
             db.sequelize.sync({alter:true});
         }
-    })
+    });
 
 
 }
---
+++ src/utils/errs/service-error.js
@@ -0,0 +1,18 @@
+const {StatusCodes}=require('http-status-codes');
+
+class ServiceError extends Error{
+    constructor(
+        message="Something Went wrong",
+        explanation="Service layer error",
+        statusCodes=StatusCodes.INTERNAL_SERVER_ERROR
+    )
+    {
+        super();
+        this.name="ServiceError";
+        this.message=message;
+        this.explanation=explanation;
+        this.statusCodes=statusCodes;
+    }
+}
+
+module.exports=ServiceError; 
